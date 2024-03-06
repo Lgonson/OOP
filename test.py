160941b73d73bfff2055366abf39db036bef1380
lucas <lgonson@gmail.com>
2024-03-06 22:22:04 +1300

    Working hard

--- test.py
+++ test.py
@@ -1,15 +1,62 @@
+import csv
+
 class Item:
-    def __init__(self, name):
+#Class Level
+    pay_rate = 0.8
+    all = []
+    def __init__(self, name: str, price: float, quantity = 0):
+        #Run validations to the received arguments
+        assert price >= 0, f"Price is lower than 0"
+        assert quantity >= 0, f"Quantity is lower than 0"
+
+    #Assign to self object
         self.name = name
-        print(f"An instance created from instance: {name}")
+        self.price = price
+        self.quantity = quantity
+
+        #Actions to execute
+        Item.all.append(self)
+
+    def calculate_revenues(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('csv.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity'))
+            )
+    @staticmethod
+    def is_integer(num):
+        #We will count out the floats that are point zero.
+        #For i.e.: 5.0, 10.0
+        if isinstance(num, float):
+            #Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item({self.name}, {self.price}, {self.quantity})"
 
 
-item1 = Item("Phone")
-#item1.name = "Phone"
-item1.price = 100
-item1_quantity = 5
+print(Item.is_integer(7.5))
+print(Item.is_integer(8.0))
 
-item2 = Item("Laptop")
-#item2.name = "Laptop("
-item2_price = 1000
-item2.quantity = 3git
+
+
+
+
+
+
