9cd6df9911dffcf9dd41a771d72b300b4c2c6628
lucas <lgonson@gmail.com>
2024-03-07 18:56:17 +1300

    working

---
+++ helper.py
@@ -0,0 +1,2 @@
+#When to use class methods and when to use static methods?
+
--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import csv
 
+
 class Item:
-#Class Level
+    # Class Level
     pay_rate = 0.8
     all = []
-    def __init__(self, name: str, price: float, quantity = 0):
-        #Run validations to the received arguments
+
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
         assert price >= 0, f"Price is lower than 0"
         assert quantity >= 0, f"Quantity is lower than 0"
 
-    #Assign to self object
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
-        #Actions to execute
+        # Actions to execute
         Item.all.append(self)
 
     def calculate_revenues(self):
@@ -27,20 +29,21 @@ class Item:
     def instantiate_from_csv(cls):
         with open('csv.csv', 'r') as f:
             reader = csv.DictReader(f)
-            items = list(reader)
+        items = list(reader)
 
         for item in items:
             Item(
-                name = item.get('name'),
-                price = float(item.get('price')),
-                quantity = int(item.get('quantity'))
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
             )
+
     @staticmethod
     def is_integer(num):
-        #We will count out the floats that are point zero.
-        #For i.e.: 5.0, 10.0
+        # We will count out the floats that are point zero.
+        # For i.e.: 5.0, 10.0
         if isinstance(num, float):
-            #Count out the floats that are point zero
+            # Count out the floats that are point zero
             return num.is_integer()
         elif isinstance(num, int):
             return True
@@ -48,14 +51,28 @@ class Item:
             return False
 
     def __repr__(self):
-        return f"Item({self.name}, {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}({self.name}, {self.price}, {self.quantity})"
+        #return f"Item(self.name}, {self.price}, {self.quantity})" we replaced this command to avoid getting allways the name of the class Item.
+
+class Phone(Item):
+    all = []
+
+    def __init__(self, name, price, quantity, broken_phones=0):
+        # Call to super function to have acces to all attributes and methods
+        super().__init__(
+            name, price, quantity
+        )
+        # Run validations to received arguments
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not greater or equal than 0"
+
+        #Actions to execute
+        Phone.all.append(self)
 
 
-print(Item.is_integer(7.5))
-print(Item.is_integer(8.0))
-
-
-
+phone1 = Phone("jscPhonev10", 500, 5)
+phone2 = Phone("jscPhonev20", 700, 5)
+print(Item.all)
+print(Phone.all)
 
 
 
